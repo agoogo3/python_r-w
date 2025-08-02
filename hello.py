d863116b6618f90696f085ace235017d3d026a0b
Junior <junioragoogo3@gmail.com>
2025-08-02 12:27:02 +0000

    Added a few comments

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import random, glob,re
 
-
+# The list of random questions
 list_quest = [ "How old are you: ",
     "What is your Favorite color: ",
     "What is your Favorite Soccer team: ",
@@ -9,26 +9,32 @@ list_quest = [ "How old are you: ",
     "Which city do you live in: ",
     "What is your Favorite game: ",
     "Who is your Favorite actor: ",
-    "Who is your Favorite musician: "
-   
+    "Who is your Favorite musician: " 
 ]
 
+# Initialize an empty list to store both the answers and the answers in a statement form
 list_answers= []
 answer_statement = []
+
+# A function that produces three random numbers from 0 to the length of the list_quest
+#It returns an array of three random numbers btw
 def questions():
     questions_index = random.sample(range(len(list_quest)),3)
     return questions_index
 
+# A function that loops through the list_qustion with the random indexes provided by the question() functiton
 def choices():
     list_answers.clear()
     answer_statement.clear()
+    # Name should only be the first name and contain only alphabets
     while True:
         name = input("What is your name: ")
         if re.fullmatch("[A-Za-z]+",name):
             break
-        print("Only First name with letters")
+        print("Only First name with letters") 
     list_answers.append(name)
     answer_statement.append(f"Hello {name}!")
+    # Loop through the questions, print it to the user and add the answers provided to the answer_input list
     for question in questions():
         print(list_quest[question])
         answer_input = input()
@@ -43,7 +49,7 @@ def choices():
         options()
         
         
-
+# A function that tallies each answer given in choices() to its corresponding statement
 def answers_statement(index, answer):
     statements = [
         f"You are {answer} years old",
@@ -59,6 +65,8 @@ def answers_statement(index, answer):
     if 0 <= index < len(statements):
         answer_statement.append(statements[index])
 
+# A function that saves the users info as a txt file.
+# It accepts the firstname and a joined form of the answer_statement lis(converted list to string)
 def save_info(firstname, statement):
     try:
         with open(f"{firstname}.txt", 'x') as f:
@@ -68,6 +76,7 @@ def save_info(firstname, statement):
     except FileExistsError:
         print("Error: User already exists. Please try again ")
 
+# A function that reads the contents of the name of a .txt file provided
 def read_user(name):
     try:
         with(open(f"{name}.txt",'r',encoding='utf-8')) as file:
@@ -77,6 +86,7 @@ def read_user(name):
         response = [False,"Error: Name not found."]
     return response
 
+# A function that locates all files that ends with .txt
 def view_users():
     files = glob.glob("*.txt")
     print("\n".join(files))
@@ -101,7 +111,7 @@ def view_users():
 
 
 
-
+# The starting function
 def options():
     print("Hello there, How can we help you? ")
     while True:
